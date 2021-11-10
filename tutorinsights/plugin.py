5eac401d7c73c95fc050ea5b18f8b74b8d61db49
uetuluk <36725861+uetuluk@users.noreply.github.com>
2021-11-10 16:42:02 +0800

    Add the variables

--- tutorinsights/plugin.py
+++ tutorinsights/plugin.py
@@ -9,7 +9,36 @@ templates = pkg_resources.resource_filename(
 )
 
 config = {
-    
+    "add": {
+        "INSIGHTS_MYSQL_PASSWORD": "{{ 8|random_string }}",
+        "API_AUTH_TOKEN": "{{ 8|random_string }}",
+        "SECRET_KEY": "{{ 20|random_string }}",
+        "INSIGHTS_OAUTH2_SECRET": "{{ 8|random_string }}",
+        "ANALYTICSAPI_OAUTH2_SECRET": "{{ 8|random_string }}",
+        "INSIGHTS_BACKEND_OAUTH2_SECRET": "{{ 8|random_string }}",
+        "ANALYTICSAPI_BACKEND_OAUTH2_SECRET": "{{ 8|random_string }}",
+        "SEGMENT_WRITE_KEY": "{{ 20|random_string }}",
+    },
+    "defaults": {
+        "VERSION": __version__,
+        "INSIGHTS_DOCKER_IMAGE": "{{ DOCKER_REGISTRY }}edxops/insights:latest",
+        "ANALYTICSAPI_DOCKER_IMAGE": "{{ DOCKER_REGISTRY }}edxops/analytics_api:latest",
+        "INSIGHTS_HOST": "insights.{{ LMS_HOST }}",
+        "ANALYTICSAPI_HOST": "analyticsapi.{{ LMS_HOST }}",
+        "INDEX_OVERRIDES": {},
+        "DASHBOARD_MYSQL_DATABASE": "dashboard",
+        "ANALYTICSAPI_MYSQL_DATABASE": "analytics-api",
+        "REPORTS_MYSQL_DATABASE": "reports",
+        "INSIGHTS_MYSQL_USER": "analytics001",
+        "ANALYTICSAPI_MYSQL_USER": "api001",
+        "INSIGHTS_OAUTH2_KEY": "insights-sso-key",
+        "INSIGHTS_BACKEND_OAUTH2_KEY": "insights-backend-service-key",
+        "ANALYTICSAPI_OAUTH2_KEY": "analytics_api-sso-key",
+        "ANALYTICSAPI_BACKEND_OAUTH2_KEY": "analytics_api-backend-service-key",
+        "CACHE_REDIS_DB": "{{ OPENEDX_CACHE_REDIS_DB }}",
+        "INSIGHTS_ELASTICSEARCH_INDEX": "learner",
+        "INSIGHTS_ELASTICSEARCH_UPDATE_INDEX": "index_update",
+    },
 }
 
 hooks = {
